2e31eada8fd70bbb16ad349db2a00118cdca5c8b
MicahJr <20954758+micahhaywoodjr@users.noreply.github.com>
2019-10-12 21:14:24 -0700

    ggrgr

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,18 +1,22 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.getElementById("p2Display");
+var numInput = document.querySelector("input");
+var winDisplay = document.getElementById("winDisplay");
 var p1Score = 0;
 var p2Score = 0;
-var gamOver = false;
+var gameOver = false;
 var winningScore = 5;
 
 console.log(p1Display);
 
 p1Button.addEventListener("click", function(){
-    if(!gamOver){
+    if(!gameOver){
         p1Score++;
         if(p1Score === winningScore){
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         console.log(p1Score);
@@ -23,13 +27,34 @@ p1Button.addEventListener("click", function(){
 
 });
 p2Button.addEventListener("click", function(){
-    if(!gamOver){
+    if(!gameOver){
     p2Score++;
     if(p2Score === winningScore){
         gameOver = true;        
+        p2Display.classList.add("winner");
     }
     p2Display.textContent = p2Score;
     }
 
-})
-console.log(p2Button);
+});
+
+resetButton.addEventListener("click",function(){
+    if(gameOver === true){ 
+    p2Score = 0;
+    p1Score = 0;
+    p1Display.textContent = p1Score;
+    p1Display.classList.remove("winner");
+    p2Display.textContent = p2Score;
+    p2Display.classList.remove("winner");
+    
+    gameOver = false;
+    }
+   
+
+});
+
+numInput.addEventListener("change", function(){
+    winDisplay.textContext = numInput.value;
+    winningScore = numInput.value;
+    
+});
