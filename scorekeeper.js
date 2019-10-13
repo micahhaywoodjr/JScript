034c9887298c6df5a0ab1492296bf8c7d410c874
MicahJr <20954758+micahhaywoodjr@users.noreply.github.com>
2019-10-12 17:18:48 -0700

    test

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,13 +1,35 @@
 var p1Button = document.querySelector("#p1");
-var p2Button = document.getElementById("p1");
-var ps1Dispay = document.querySelector("#ps1Dispay");
-var h1 = document.querySelector("h1");
+var p2Button = document.getElementById("p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.getElementById("p2Display");
+var p1Score = 0;
+var p2Score = 0;
+var gamOver = false;
+var winningScore = 5;
 
-
-var  p1Score = 0;
+console.log(p1Display);
 
 p1Button.addEventListener("click", function(){
-    p1Score ++;
-    ps1Dispay.textContent = p1Score;
+    if(!gamOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            gameOver = true;
+        }
+        console.log(p1Score);
+        p1Display.textContent = p1Score;
+    }
+ 
+
+
+});
+p2Button.addEventListener("click", function(){
+    if(!gamOver){
+    p2Score++;
+    if(p2Score === winningScore){
+        gameOver = true;        
+    }
+    p2Display.textContent = p2Score;
+    }
 
 })
+console.log(p2Button);
